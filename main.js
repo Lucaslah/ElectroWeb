3c837b450049aa4f91047a979ecbae8c8aa3dcb3
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-04-17 23:01:27 +1200

    Don’t quick app when all tabs are closed for macOS

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
 
 app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin') {
   app.quit();
+  }
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 1024, height: 768, webPreferences: {nodeIntegration: true, webviewTag: true} });
+  mainWindow = new BrowserWindow({ width: 1024, height: 768, webPreferences: { nodeIntegration: true, webviewTag: true } });
   mainWindow.loadURL('file://' + __dirname + '/browser.html');
 });
