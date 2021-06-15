25a9d325f6a94e8a72cf900f043974cb1af71d64
lucaslah <47047328+Lucaslah@users.noreply.github.com>
2021-06-16 00:39:43 +1200

    Update createWindow.ts

--- src/createWindow.ts
+++ src/createWindow.ts
@@ -1,1 +1,17 @@
-import { BrowserWindow } from "electron";
+import {
+    BrowserWindow
+} from "electron";
+
+export default class createWindow {
+    constructor() {}
+
+    public init() {
+        const mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+                preload: path.join(__dirname, 'preload.js')
+            }
+        });
+    }
+}
